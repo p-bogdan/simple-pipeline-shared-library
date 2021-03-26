1498577900aeb39c275a80ad3e345e60c2ab2754
Petro Bogdan <you@example.com>
2021-03-26 13:24:16 +0200

    Testing some changes

--- vars/simple-pipeline.groovy
+++ vars/simple-pipeline.groovy
@@ -1,24 +1,7 @@
-def call(buildPlan = [:]) {
-  node(buildPlan.jenkinsNode) {
-    stage('Initialize terraform') {
-            steps {
-                echo 'Initialize terraform'
-                sh "terraform init"
-                
-                /*archiveArtifacts artifacts: 'dist/trainSchedule.zip'*/
-            }
-        }
-    stage('Create GCP instance') {
-            steps {
-            echo 'Creating compute instance in GCP'
-            sh 'terraform apply --auto-approve'
-              
-            }
-        }
-    /*if (buildPlan.npm.tslint) {
-      stage("TSlint") {
-        sh "npm run tslint"
-      }
-    }*/
-  }
+def call(Map config) {
+    node {
+        git url: "https://github.com/p-bogdan/simple-pipeline-shared-library"
+        //sh 'mvn install'
+        echo "Hello World"
+    }
 }
