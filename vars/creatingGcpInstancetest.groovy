0189abdd8367dd8a63a389b746fdc4c895058211
Petro Bogdan <you@example.com>
2021-03-29 17:31:38 +0300

    Testing some changes

--- vars/creatingGcpInstancetest.groovy
+++ vars/creatingGcpInstancetest.groovy
@@ -35,15 +35,17 @@ def call(Map config=[:], Closure createGCPinstance) {
               
         }
       }
-      /*  stage('Destroy GCP instance') {
+        stage('Destroy GCP instance') {
+            if (config.destroy_instance) {
             steps {
             timeout(time: 5, unit: 'MINUTES') {
             input(id: "Destroying terraform instance", message: "Are you sure to destroy ${params.project_name}?", ok: 'Destroy')
             echo 'Destroying GCP instance'
             sh 'terraform destroy -force'
             }
+            }
         }
-      }*/
+      }
     }
   }
 }
