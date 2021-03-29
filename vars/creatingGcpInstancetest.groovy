9e828cecb046350a5e171986f2369e69a6624490
Petro Bogdan <you@example.com>
2021-03-29 15:59:47 +0300

    Testing some changes

---
+++ vars/creatingGcpInstancetest.groovy
@@ -0,0 +1,19 @@
+def call(Map config=[:], Closure body) {
+    node {
+        git url: "https://github.com/p-bogdan/simple-pipeline-shared-library.git"
+        stage("Initializing terraform") {
+          echo 'Initialize terraform'
+          sh   "terraform init"
+        }
+        stage("Create GCP instance") {
+            echo 'Creating compute instance in GCP'
+            sh 'terraform apply --auto-approve'
+        } 
+        /*stage("Deploy") {
+            if (config.deploy) {
+                sh "npm publish"
+            }
+        }*/
+        body()
+    }
+}
