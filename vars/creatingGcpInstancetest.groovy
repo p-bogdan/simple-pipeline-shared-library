00e1b2dc807459a9647520ec48e53af79e74b4a1
Petro Bogdan <you@example.com>
2021-03-29 17:27:03 +0300

    Testing some changes

--- vars/creatingGcpInstancetest.groovy
+++ vars/creatingGcpInstancetest.groovy
@@ -1,4 +1,4 @@
-def call(Map config=[:], Closure body) {
+/*def call(Map config=[:], Closure body) {
     node {
         git url: "https://github.com/p-bogdan/simple-pipeline-shared-library.git"
         stage("Initializing terraform") {
@@ -16,4 +16,34 @@ def call(Map config=[:], Closure body) {
         }
         body()
     }
+}*/
+
+def call(Map config=[:], Closure createGCPinstance) {
+  pipeline {
+    agent any
+    stages {
+      stage('Initializing terraform') {
+        steps {
+          echo 'Initialize terraform'
+          sh   "terraform init"
+        }
+      }
+        stage('Create GCP instance') {
+            steps {
+            echo 'Creating compute instance in GCP'
+            sh 'terraform apply --auto-approve'
+              
+        }
+      }
+      /*  stage('Destroy GCP instance') {
+            steps {
+            timeout(time: 5, unit: 'MINUTES') {
+            input(id: "Destroying terraform instance", message: "Are you sure to destroy ${params.project_name}?", ok: 'Destroy')
+            echo 'Destroying GCP instance'
+            sh 'terraform destroy -force'
+            }
+        }
+      }*/
+    }
+  }
 }
