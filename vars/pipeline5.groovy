c6d3c488fa62741462ec05522f822102d597b108
Petro Bogdan <you@example.com>
2021-03-29 17:09:14 +0300

    Testing some changes

--- vars/pipeline5.groovy
+++ vars/pipeline5.groovy
@@ -1,3 +1,9 @@
 def uploadToBucket() {
   googleStorageUpload bucket: 'gs://simple-pipeline-artifact2', credentialsId: 'simple-pipeline-app', pattern: 'app/build/distributions/*'
 }
+
+def downloadFromBucket() {
+  step([$class: 'DownloadStep', credentialsId: env
+                        .CREDENTIALS_ID,  bucketUri: "gs://${env.BUCKET}/${env.PATTERN}",
+                      localDirectory: env.LOCAL_DIR])
+}
